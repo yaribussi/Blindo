bd6c56e27c5e7fea63760c17104773bf90cac0c4
Yari_Bussi <yaribussi@gmail.com>
2019-07-22 10:24:52 +0200

    refactor GUIKeyboard code

--- GUIkeyboard.py
+++ GUIkeyboard.py
@@ -10,24 +10,26 @@ import tkinter
 
 
 buttons = [
-'1','2','3','4','5','6','7','8','9','0','?',')','-','INVIO',
-'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','!','$','£','BACK',
-'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','[',']','_','+','!'
+'1','2','3','4','5','6','7','8','9','0','(',')','$','CANCELLA',
+
+'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p','[',']','€','SALVA',
+
+'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l','/','!','?','"',''
 ,
-'z', 'x', 'c', 'v', 'b', 'n', 'm', '_', '.','#','-','-','+',
-'SPACE'
+'z', 'x', 'c', 'v', 'b', 'n', 'm', '_', '-','#','*','+','.',
+'SPAZIO'
 ]
 result=""
 def select(value):
 
-	if value == "BACK":
+	if value == "CANCELLA":
 		# allText = entry.get()[:-1]
 		# entry.delete(0, tkinter,END)
 		# entry.insert(0,allText)
 
 		entry.delete(len(entry.get())-1,tkinter.END)
 
-	elif value == "SPACE":
+	elif value == "SPAZIO":
 		entry.insert(tkinter.END, ' ')
 	elif value == " Tab ":
 		entry.insert(tkinter.END, '    ')
@@ -42,6 +44,12 @@ def close(root):
         root.destroy()
 
 def HosoPop(kb):
+	key_background = "orange2"				#"#3c4987"
+	key_foreground = "black"				#"#ffffff"
+	key_active_background = "orange"		#"#ffffff"
+	key_active_foreground = "black"			#"#3c4987"
+	key_relief = 'raised'
+	key_font = ("Helvetica", 13)
 
 	varRow = 2
 	varColumn = 0
@@ -50,27 +58,54 @@ def HosoPop(kb):
 
 		command = lambda x=button: select(x)
 
-		if button == "INVIO":
-			tkinter.Button(kb, text=button, width=15,heigh=6, bg="#3c4987", fg="#ffffff",
-					   activebackground="#ffffff", activeforeground="#3c4987", relief='raised',
-                                               font=("Helvetica",13), padx=1,
-					   pady=1, bd=5, command=lambda:close(kb)).grid(row=6,column=0 ,columnspan=3)
+		if button == "CANCELLA":
+			tkinter.Button(
+						kb, text=button, width=15, heigh=6,
+						bg=key_background,
+						fg=key_foreground,
+						activebackground=key_active_background,
+						activeforeground=key_active_foreground,
+						relief=key_relief,
+						font=key_font,
+						padx=1, pady=1,
+						bd=5,
+						command=command).grid(row=6, column=0, columnspan=3)
 
-		elif button == "SPACE" :
-			tkinter.Button(kb, text=button, width=30,heigh=6, bg="#3c4987",
-                                       font=("Helvetica",13), fg="#ffffff",
-						   activebackground="#ffffff", activeforeground="#3c4987", relief='raised', padx=1,
-						   pady=1, bd=5, command=command).grid(row=6,column=1 ,columnspan=9)
-		elif button == "BACK" :
-			tkinter.Button(kb, text=button, width=15,heigh=6, bg="#3c4987",
-                                       font=("Helvetica",13), fg="#ffffff",
-						   activebackground="#ffffff", activeforeground="#3c4987", relief='raised', padx=1,
-						   pady=1, bd=5, command=command).grid(row=6, column=8,columnspan=3)
+		elif button == "SPAZIO":
+			tkinter.Button(
+						kb, text=button, width=30, heigh=6,
+						bg=key_background,
+						font=key_font, fg=key_foreground,
+						activebackground=key_active_background,
+						activeforeground=key_active_foreground,
+						relief=key_relief,
+						padx=1, pady=1,
+						bd=5,
+						command=command).grid(row=6, column=1, columnspan=9)
+		elif button == "SALVA" :
+			tkinter.Button(
+						kb, text=button, width=15, heigh=6,
+						bg=key_background,
+						font=key_font,
+						fg=key_foreground,
+						activebackground=key_active_background,
+						activeforeground=key_active_background,
+						relief=key_relief,
+						padx=1, pady=1,
+						bd=5,
+						command=lambda: close(kb)).grid(row=6, column=8, columnspan=3)
 		else:
-			tkinter.Button(kb,text= button,width=5,heigh=2,
-                                       font=("Helvetica",15),bg="#3c4987", fg="#ffffff",
-				activebackground = "#ffffff", activeforeground="#3c4987", relief='raised', padx=1,
-				pady=1, bd=6,command=command).grid(row=varRow,column=varColumn)
+			tkinter.Button(
+						kb, text=button, width=5, heigh=2,
+						font=key_font,
+						bg=key_background,
+						fg=key_foreground,
+						activebackground = key_active_background,
+						activeforeground=key_active_foreground,
+						relief=key_relief,
+						padx=1, pady=1,
+						bd=6,
+						command=command).grid(row=varRow, column=varColumn)
 
 
 		varColumn +=1
@@ -85,20 +120,32 @@ def HosoPop(kb):
 			varColumn = 0
 			varRow+=1
 
-
-
 def keyBoard():
+	label_font = ("Helvetica",20)
+	label_bg_color = "red"
+	label_text = "Scegli con che nome vuoi salvare il file"
+
+	entry_font = ("Helvetica",30)
+	entry_background = "white"
+	entry_foreground = "black"
 	kb = tkinter.Tk()
 
 	kb.attributes('-fullscreen', True)
-
-
-	label1 = Label(kb,text="Sciegli con che nome vuoi salvare il file",font=("Helvetica",20)).grid(row=0,columnspan=11)
+	kb.config(bg="red")
+	label_scritta=Label(
+						kb,
+						text=label_text,
+						font=label_font,
+						bg=label_bg_color)
+	label_scritta.grid(row=0, columnspan=11)
 
 	global entry
 	entry = Entry(kb,width=30)
-	entry.config(font=("Helvetica",30))
-	entry.grid(row=1,columnspan=11)
+	entry.config(
+				font=entry_font,
+				bg=entry_background,
+				fg=entry_foreground)
+	entry.grid(row=1, column=0, columnspan=12)
 
 	HosoPop(kb)
 
@@ -108,3 +155,5 @@ def keyBoard():
 	return result
 
 
+
+print(keyBoard())
